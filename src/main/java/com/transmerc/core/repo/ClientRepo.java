1d1c1d5caebfa4902ada5fd61cecf00906990300
Jorge Hurtado <jjhurtado796@gmail.com>
2021-10-12 13:19:38 -0400

    CREATED: Cliente UC and Repo

---
+++ src/main/java/com/transmerc/core/repo/ClientRepo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.transmerc.core.repo;
+
+import com.root101.clean.core.app.repo.CRUDRepository;
+import com.transmerc.core.domain.Client;
+
+/**
+ *
+ * @author Jorge
+ */
+public interface ClientRepo extends CRUDRepository<Client>{ 
+}
---
+++ src/main/java/com/transmerc/core/usecase/impl/ClientUseCaseImpl.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.transmerc.core.usecase.impl;
+
+import com.root101.clean.core.app.usecase.DefaultCRUDUseCase;
+import com.transmerc.core.domain.Client;
+import com.transmerc.core.usecase.ClientUseCase;
+
+public class ClientUseCaseImpl extends DefaultCRUDUseCase<Client>
+        implements ClientUseCase {
+
+}
